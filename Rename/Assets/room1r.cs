d3018cdb0d6cb282f2f5cbe38cf0f8540b38dd3e
Coolp-GitHub <mohanparasinder@gmail.com>
2020-12-27 14:08:19 -0500

    Doors start

---
+++ Rename/Assets/room1r.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class room1r : MonoBehaviour
+{
+    public Transform player;
+    public Transform room;
+
+    public Vector2 doorPos;
+
+
+    void Update()
+    {
+        doorPos = new Vector2(room.position.x + 20, room.position.y);
+    }
+   
+
+    void OnTriggerEnter2D(Collider2D Wall)
+    {
+       
+
+        if (Mathf.Abs(player.position.x - doorPos.x) < 50  && Mathf.Abs(player.position.y - doorPos.y) < 50)
+        {
+            Wall.enabled = false;
+        }
+        else
+        {
+            Wall.enabled = true;
+        }
+    }
+}
