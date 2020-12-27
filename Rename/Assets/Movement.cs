6e8641d8cce26b9ec6e7b67b9a8585e72be7a941
Coolp-GitHub <mohanparasinder@gmail.com>
2020-12-26 22:12:45 -0500

    Added a map

---
+++ Rename/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+
+    Vector2 move;
+
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.position = rb.position + move * speed * Time.deltaTime;
+    }
+}
---
+++ Rename/Assets/door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public Transform room;
+    public Rigidbody2D rbP;
+    public Vector2 Entry = (room.x + 19, room.y);
+    
+
+    void OnTriggerEnter2D()
+    {
+       
+    }
+}
